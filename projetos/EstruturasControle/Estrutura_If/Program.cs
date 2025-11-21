000568c8ab939f4a8c3ecb8e3bd9b7e78297c841
ducrz <ducatolico@gmail.com>
2025-11-21 19:54:25 -0300

    Criação de solução EstruturasControle

---
+++ projetos/EstruturasControle/Estrutura_If/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("Instrução If");
+
+Console.WriteLine("Cliente Especial (S/N)");
+var resposta = Console.ReadLine();
+
+if(resposta == "S")
+{
+    Console.WriteLine("Desconto de 10%");
+}
+
+Console.ReadKey();
+
+/* Usando True or false */
+
+Console.WriteLine("Cliente Especial (true/false)");
+var resposta = Console.ReadLine();
+
+if(resposta)
+{
+    Console.WriteLine("Desconto de 10%");
+}
+Console.WriteLine("Fim do Processamento!");
+Console.ReadKey();
+
+/* Comparar se x é maior, menor ou igual a y */
+Console.Write("Informe o valor de x \t");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nInforme o valor de y \t");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("x é maior que y");
+}
+if (x < y)
+{
+    Console.WriteLine("x é menor que y");
+}
+if (x == y)
+{
+    Console.WriteLine("x é igual a y");
+}
+
+
+Console.ReadKey();
+
+
+
+
+
+
+
---
+++ projetos/EstruturasControle/Estrutura_IfElse/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Instrução IF-Else
+A instrução if-else é composta por dois blocos de instruções: o bloco de
+instruções if e o bloco de instruções else.
+
+*/
+Console.WriteLine("Estrutura de Controle IF-ELSE");
+
+Console.ReadLine("Informe a nota do aluno \t");
+var nota = Convert.ToInt32(Console.ReadLine());
+
+
+if (nota > 5)
+{
+    Console.WriteLine("Aluno aprovado!");
+}
+else
+{
+    Console.WriteLine("Aluno reprovado!");
+
+}
+Console.ReadKey();
+
+/* Comparar se x é maior, menor ou igual a y */
+Console.ReadLine("Informe o valor de x \t");
+var x= Convert.ToInt32(Console.ReadLine());
+Console.ReadLine("Informe o valor de y \t");
+var y= Convert.ToInt32(Console.ReadLine());
+
+
+if (x > y)
+{
+    Console.WriteLine("x é maior que y!");
+}
+else
+{
+    if (x < y) //Solução com if-else aninhados
+    {
+        Console.WriteLine("x é menor que y!");
+    }
+    else
+    {
+        Console.WriteLine("x é igual a y!");
+
+    }
+}
+Console.ReadKey();
