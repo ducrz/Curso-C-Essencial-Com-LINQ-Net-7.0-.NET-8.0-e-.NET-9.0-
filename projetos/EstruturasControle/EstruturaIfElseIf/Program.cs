eccb9a3b47cbcd81d027160fae64f9ccdc8fa3ae
ducrz <ducatolico@gmail.com>
2025-11-22 16:34:53 -0300

    Inclusão EstruturasControle e códigos de aulas relacionadas.

---
+++ projetos/EstruturasControle/EstruturaIfElseIf/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Instrução If-Else-If
+
+Podemos usar instruções else if após a instrução if para avaliar mais de uma condição.
+As instruções else if somente serão executadas se a condição na instrução if for falsa.
+
+Portanto: a instrução if será executada ou uma das instruções else if será executada,
+mas não ambas.
+
+A instrução if pode ter várias cláusulas else if onde cada cláusula tem uma condição.
+
+A instrução else if verifica cada condição de cima para baixo sequencialmente,
+e, se uma condição for verdadeira, o bloco correspondente é executado e as demais 
+condições não serão avaliadas.
+
+if (condicao1)
+{
+    //executa se condicao1 for true
+    //instrucao
+}
+else if (condicao2)
+{
+    //executa se condicao1 for false e condicao2 for true
+    //instrucao    
+}
+else if (condicao3)
+{
+    //executa se condicao1 for false, condicao2 for false e condicao3 for true
+    //instrucao
+}
+else
+{
+    //executa se todas as condicoes anteriores forem false
+    //instrucao
+}
+//Possível utilizar sem o else final
+*/
+Console.WriteLine("Instrução If-Else-if");
+
+/* Aluno reprovado, recuperação, aprovado e aprovado com distinção
+Informar a nota do aluno
+
+se nota menor que 5.0 -> reprovado
+se nota entre 5.0 e 6.0 -> recuperação
+se nota maior ou igual a 6 e menor que 9.0 -> aprovado
+se nota maior ou igual a 9.0 -> aprovado com distinção
+
+*/
+Console.Write("Informe a nota do aluno: \t");
+double nota = Convert.ToDouble(Console.ReadLine()); 
+if (nota < 5.0)
+{
+    Console.WriteLine("\nAluno reprovado.");
+}
+else if (nota >= 5.0 && nota < 6.0)
+{
+    Console.WriteLine("\nAluno em recuperação.");
+}
+else if (nota >= 6.0 && nota < 9.0)
+{
+    Console.WriteLine("\nAluno aprovado.");
+}
+else if(nota >= 9.0) //nota >= 9.0
+{
+    Console.WriteLine("\nAluno aprovado com distinção.");
+}
+Console.ReadKey();
---
+++ projetos/EstruturasControle/EstruturaSwitch/Program.cs
@@ -0,0 +1,150 @@
+﻿/*
+Estrutura Switch em C#
+
+O bloco switch case é uma estrutura de condição que define o código
+a ser executado com base em uma comparação de valores.
+
+Exemplo simples de uso do switch case em C#:
+switch(variavel/expressao)
+{
+    case valor1:
+        // código a ser executado se variavel/expressao == valor1
+        break;
+    case valor2:
+        // código a ser executado se variavel/expressao == valor2
+        break;
+    ...
+    default:
+        // código a ser executado se nenhum dos casos anteriores for atendido
+        break;
+}
+Exemplo de número de prestações:
+
+informar o número de prestações (1 a n)
+
+Avalia o valor informado
+
+Caso valor igual a 1
+prestação 300
+sair
+
+Caso valor igual a 2
+prestacao 150
+sair
+Caso valor igual a 3
+prestação 100
+sair
+Padrão
+Número de prestações inválido
+Sair
+*/
+Console.WriteLine("Estrutura Switch em C#");
+int compra = 600;
+
+Console.WriteLine("Valor da compra: R$ 600,00\n");
+Console.Write("Informe o número de prestações (1 a 3): ");
+int numeroPrestacoes = Convert.ToInt32(Console.ReadLine());
+switch (numeroPrestacoes)
+{
+    case 1:
+        Console.WriteLine($"\nPrestação R$ {600/numeroPrestacoes }");
+        break;
+    case 2:
+        Console.WriteLine($"\nPrestação R$ {600/numeroPrestacoes }");
+        break;
+    case 3:
+        Console.WriteLine($"\nPrestação R$ {600/numeroPrestacoes }");
+        break;
+    default:
+        Console.WriteLine("Valor inválido, informe um número entre 1 e 3.");
+        break;
+}   
+
+//Avaliando o valor de uma expressão: Par ou ímpar
+
+Console.WriteLine("Informe um número inteiro:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+switch (numero % 2)
+{
+    case 0:
+        Console.WriteLine($"\nO número {numero} é par.");
+        break;
+    case 1:
+        Console.WriteLine($"\nO número {numero} é ímpar.");
+        break;
+    default:
+        Console.WriteLine("Erro na avaliação do número.");
+        break;
+}
+
+/*
+Executar o mesmo código para valores diferentes
+
+Exemplo - dias dos meses do ano (2026)
+*/
+Console.WriteLine("\nInforme o nome do mês para saber a quantidade de dias:");
+var mes = Console.ReadLine().ToLower();
+
+switch (mes)
+{
+    case "janeiro":
+    case "março":
+    case "maio":
+    case "julho":
+    case "agosto":
+    case "outubro":
+    case "dezembro":
+        Console.WriteLine($"\nO mês de {mes} tem 31 dias.");
+        break;
+    case "abril":
+    case "junho":
+    case "setembro":
+    case "novembro":
+        Console.WriteLine($"\nO mês de {mes} tem 30 dias.");
+        break;
+    case "fevereiro":
+        Console.WriteLine($"\nO mês de {mes} tem 28 dias em 2026.");
+        break;
+    default:
+        Console.WriteLine("Mês inválido.");
+        break;
+}
+
+/* Instruções switch case aninhadas 
+
+*/
+
+int cargo =0;
+int funcao =0;
+
+Console.WriteLine("Informe o código do cargo: Gerente (1)ou Programador (2)? \t");
+cargo = Convert.ToInt32(Console.ReadLine());
+
+switch (cargo)
+{
+    case 1:
+
+        Console.WriteLine("\n Bem-vindo, Gerente!");
+        break;
+    case 2:
+    Console.WriteLine("\nBem-vindo Programador!");
+    switch (funcao)
+    {
+        case 1:
+            Console.WriteLine("Função: Júnior");
+            break;
+        case 2:
+            Console.WriteLine("Função: Sênior");
+            break;
+        default:
+            Console.WriteLine("Função Desconhecida.");
+            break;
+    }
+    break;
+    default:
+        Console.WriteLine("Cargo Desconhecido.");
+        break;  
+}
+
+Console.ReadKey();
