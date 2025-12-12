9ac24dc7097700fbf5a1d39c8304449f23f0cc9b
ducrz <ducatolico@gmail.com>
2025-12-12 14:33:40 -0300

    Fim Seção 11: Serialização e Desserialização

---
+++ projetos/Serializacao/SerializacaoExercicio/Aluno.cs
@@ -0,0 +1,18 @@
+﻿namespace Serializacao_Exercicio;
+
+public class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Idade;
+    public Aluno()  //acrescente o construtor sem parâmetros
+    { }
+    public Aluno(int id, string nome, string email, int idade)
+    {
+        Id = id;
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+}
---
+++ projetos/Serializacao/SerializacaoExercicio/Program.cs
@@ -0,0 +1,43 @@
+﻿﻿using Serializacao_Exercicio;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+//cria lista de alunos
+List<Aluno> alunos = new()
+{
+    new Aluno { Id=1, Nome="Maria", Email= "maria@email.com", Idade=16},
+    new Aluno { Id=2, Nome="Carlos", Email= "carlos@email.com", Idade=17},
+    new Aluno { Id=3, Nome="Silvia", Email= "silvia@email.com", Idade=15},
+};
+
+Console.WriteLine("\nRealizando a serialização JSON\n");
+
+//serialização JSON
+var listaJson = JsonSerializer.Serialize(alunos);
+
+Console.WriteLine(listaJson);
+
+var caminhoArquivoJSON = @"d:\dados\Serializados\listaAlunos.json";
+
+using (FileStream stream = new FileStream(caminhoArquivoJSON, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+{
+    JsonSerializer.Serialize(stream, alunos);
+}
+
+Console.WriteLine("\nSerialização JSON concluída. Tecle algo para continuar...");
+
+Console.ReadKey();
+
+Console.WriteLine("\nRealizando a serialização XML\n");
+
+var caminhoArquivoXML = @"d:\dados\Serializados\listaAlunos.xml";
+
+var listaSerializer = new XmlSerializer(typeof(List<Aluno>));
+using (var writer = new StreamWriter(caminhoArquivoXML))
+{
+    listaSerializer.Serialize(writer, alunos);
+}
+
+Console.WriteLine("\nSerialização XML concluída. Tecle algo para encerrar...");
+
+Console.ReadKey();
